8758b7fc590e27114e106c3b0b55ba88665726a7
eekanomic <jp@eekanomic.com>
2016-08-11 10:50:23 -0400

    Click browser icon to open new tab with Bleak
    updated background.js to listen to browser icon click

---
+++ background.js
@@ -0,0 +1,28 @@
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    var requestHeaders = details.requestHeaders;
+    for (var i=0; i<requestHeaders.length; ++i) {
+        if (requestHeaders[i].name.toLowerCase() === 'referer') {
+            // The request was certainly not initiated by a Chrome extension...
+            return;
+        }
+    }
+    // Set Referer
+    requestHeaders.push({
+        name: 'referer',
+        // Host must match the domain in your Typekit kit settings
+        value: 'https://fpjagaljndnekelimchojdakbnfalejm/'
+    });
+    return {
+        requestHeaders: requestHeaders
+    };
+}, {
+    urls: ['*://use.typekit.net/*'],
+    types: ['stylesheet']
+}, ['requestHeaders','blocking'])
+
+chrome.browserAction.onClicked.addListener(function()
+{
+    chrome.tabs.create({ url: "index.html" });
+});
